1ca726fdd054043dd471a41d47e2585816cd913b
sidnny <sid@sidnny.com>
2019-02-24 15:03:28 -0500

    adding privacy policy

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/privacy', function(req, res, next) {
+  res.render('privacy', { title: 'Women Tech Leaders - Privacy Policy', pageId: 'privacy' });
+});
 router.get('/code_of_conduct', function(req, res, next) {
-  res.render('coc', { title: 'Women Tech Leaders Code of Conduct', pageId: 'code_of_conduct' });
+  res.render('coc', { title: 'Women Tech Leaders - Code of Conduct', pageId: 'code_of_conduct' });
 });
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Welcome to Women Tech Leaders - A peer group for women* in tech who are managing people and leading teams.', pageId: 'index' });
+  res.render('index', { title: 'Welcome to Women Tech Leaders - A peer group for women in tech who are managing people and leading teams.', pageId: 'index' });
 });
 
 module.exports = router;
