aac2a2ec0d5c8ee9fdac4d69a84f6e753508d03a
sidnny <sid@sidnny.com>
2019-02-25 04:44:22 +0000

    removed dry run from cert renewal

--- ssl_renew.sh
+++ ssl_renew.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-/usr/local/bin/docker-compose -f /home/sid/wtl-web/docker-compose.yml run certbot renew --dry-run \
+/usr/local/bin/docker-compose -f /home/sid/wtl-web/docker-compose.yml run certbot renew \
 && /usr/local/bin/docker-compose -f /home/sid/wtl-web/docker-compose.yml kill -s SIGHUP webserver
