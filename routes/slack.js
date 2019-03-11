76cf33eb917e3c1af399fd22508a812a8f6d65fe
sid <sid@sidnny.com>
2019-03-11 00:09:04 -0400

    Slack invite automation (#9)
    * clean up nginx conf formatting
    
    * rerouting to https://wtl-slack-invite.herokuapp.com/

--- routes/slack.js
+++ routes/slack.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.redirect(307, 'https://wtlnyc-slack.herokuapp.com');
+  res.redirect(307, 'https://wtl-slack-invite.herokuapp.com/');
 });
 
 module.exports = router;
