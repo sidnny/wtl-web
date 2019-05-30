573490be398e73905171142ec2c44f04f6e8907e
sid <sid@sidnny.com>
2019-05-29 21:27:05 -0700

    Use CDN (#16)
    * switch img urls to use cdn
    
    * define cdn path
    
    * adding NODE_ENV for prod
    
    * adding alt tags to images

--- www/routes/index.js
+++ www/routes/index.js
@@ -2,20 +2,22 @@ var axios = require('axios');
 var express = require('express');
 var router = express.Router();
 
+const CDN = (process.env.NODE_ENV === 'production') ? 'https://cdn.wtl.nyc' : '';
+
 router.get('/privacy', function(req, res, next) {
-  res.render('privacy', { title: 'Women Tech Leaders - Privacy Policy', pageId: 'privacy' });
+  res.render('privacy', { cdn: CDN, title: 'Women Tech Leaders - Privacy Policy', pageId: 'privacy' });
 });
 router.get('/code_of_conduct', function(req, res, next) {
-  res.render('coc', { title: 'Women Tech Leaders - Code of Conduct', pageId: 'code_of_conduct' });
+  res.render('coc', { cdn: CDN, title: 'Women Tech Leaders - Code of Conduct', pageId: 'code_of_conduct' });
 });
 router.get('/', function(req, res, next) {
   axios.get('https://api.meetup.com/2/events?key=105d6a6a1487e4b58676725161d183b&group_urlname=wtl-women-tech-leaders-nyc&sign=true')
     .then(function (response) {
-      res.render('index', { title: 'Welcome to Women Tech Leaders.', pageId: 'index', events: response.data.results });
+      res.render('index', { cdn: CDN, title: 'Welcome to Women Tech Leaders.', pageId: 'index', events: response.data.results });
     })
     .catch(function (error) {
       console.error(error);
-      res.render('index', { title: 'Welcome to Women Tech Leaders.', pageId: 'index' });
+      res.render('index', { cdn: CDN, title: 'Welcome to Women Tech Leaders.', pageId: 'index' });
     });
 });
 
